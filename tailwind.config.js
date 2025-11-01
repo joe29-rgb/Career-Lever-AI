f0504b0c6adc254c31e67ab40db067009f5ca804
Your Name <your.email@example.com>
2025-11-01 17:20:31 -0600

    FixTailwindConfigToMatchCleanCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,9 +53,9 @@ module.exports = {
                 },
             },
             borderRadius: {
-                lg: "var(--radius)",
-                md: "calc(var(--radius) - 2px)",
-                sm: "calc(var(--radius) - 4px)",
+                lg: "var(--radius-lg)",
+                md: "var(--radius-md)",
+                sm: "var(--radius-sm)",
             },
             keyframes: {
                 "accordion-down": {
